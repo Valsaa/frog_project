7c50aa7aab1c086d27dadedfc62c5583ca931815
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-02-26 16:05:40 +0100

    basic InpuManager
    ajout des paramètres de base de InputManager et méthode de lecture des inputs

--- Assets/Scripts/Inputmanager.cs
+++ Assets/Scripts/Inputmanager.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inputmanager : MonoBehaviour {
+public class InputManager : MonoBehaviour {
+
+	public struct Mouvement{
+		public bool up;
+		public bool down;
+		public bool left;
+		public bool right;
+	};
+	public Mouvement mouvement;
+	public bool replayModeOn = false;
+	public bool saveModeOn = false;
+	public string replayFilepath = "";
 
 	// Use this for initialization
 	void Start () {
-		
+		// setup
+		this.mouvement.up = false;
+		this.mouvement.down = false;
+		this.mouvement.left = false;
+		this.mouvement.right = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.ReadMouvement ();
+	}
+
+	private void ReadMouvement()
+	{
+		this.mouvement.right = Input.GetKey (KeyCode.D);
+		this.mouvement.left = Input.GetKey (KeyCode.Q);
+		this.mouvement.up = Input.GetKey (KeyCode.Z);
+		this.mouvement.down = Input.GetKey (KeyCode.S);
 	}
 }
