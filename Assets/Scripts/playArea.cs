a39320dc346ad27de21a81507a2e10ecfe37f363
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-03-01 09:07:58 +0100

    added basic map réduction script

---
+++ Assets/Scripts/playArea.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playArea : MonoBehaviour {
+
+	private float previousTime;
+	private float actualTime;
+	private Vector2 currentScale;
+	public int areaReductionPeriodSeconds = 10;
+	public float areaReductionScale = 1f;
+	public int areaExitDamagePerSeconds = 5;
+
+	// Use this for initialization
+	void Start () {
+		previousTime = actualTime = Time.time;
+		currentScale = this.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		actualTime = Time.time;
+		if ((actualTime - previousTime) >= areaReductionPeriodSeconds)
+		{
+			previousTime = actualTime;
+			if (currentScale.x > 10 && currentScale.y > 10)
+			{
+				currentScale = new Vector2 (currentScale.x - areaReductionScale, currentScale.y - areaReductionScale);
+				this.transform.localScale = currentScale;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.name == "boss" || col.gameObject.name == "perso")
+		{
+			// TODO
+		}
+	}
+}
