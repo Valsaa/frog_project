27009f2785379baaff874916eec61dcd3cc35b05
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-04-07 18:42:57 +0200

    ajout possibilité d'ajouter des sorts à la ZDA

--- Assets/Scripts/DangerZone.cs
+++ Assets/Scripts/DangerZone.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DangerZone : MapSizeGenerator
 {
     private int Delta = 0;
-    public 
+    public List<string> SortFileList;
+    public List<Sort> SortList;
 	// Use this for initialization
 	void Start () {
 
         this.Init();
+
+        foreach(string sortName in SortFileList)
+        {
+            StreamReader file = new StreamReader("Assets/Data/Sorts/" + sortName + ".json", false);
+            if (file.ToString() == "")
+            {
+                Debug.Log("can't get sort '" + sortName + "' can't open file (" + "Assets/Data/Sorts/" + sortName + ")");
+                return;
+            }
+            SortList.Add(JsonUtility.FromJson<Sort>(file.ReadToEnd()));
+        }
 
         UpdateDangerZone();
     }
