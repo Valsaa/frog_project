730a77f3f0383f14c961410a224971af756383f1
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-04-07 13:59:04 +0200

    ajout d'un script de comportement de camera

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    public enum CameraPositionBehaviours { FOLLOW_MAIN_CHARACTER };
+    public CameraPositionBehaviours cameraBehaviour = CameraPositionBehaviours.FOLLOW_MAIN_CHARACTER;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(cameraBehaviour)
+        {
+            default:
+            case CameraPositionBehaviours.FOLLOW_MAIN_CHARACTER:
+                Vector3 PlayerPosition = GameObject.Find("MainCharacter").GetComponent<Transform>().position;
+                Vector3 CameraPosition = this.GetComponent<Transform>().position;
+                this.GetComponent<Transform>().position = new Vector3(PlayerPosition.x, PlayerPosition.y, CameraPosition.z);
+                break;
+        }
+	}
+}
---
+++ Assets/Scripts/IA.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour {
+
+    public enum Status { NULL_0, DEP_OFF_1, AGGRO_2, DEP_DEF_3, ESQ_4, DEP_MAP_5};
+    public Status currentStatus;
+
+	// Use this for initialization
+	void Start () {
+        currentStatus = Status.NULL_0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentStatus)
+        {
+            default:
+            case Status.NULL_0:
+                // do stuff
+
+                // check stuff
+                break;
+            case Status.DEP_OFF_1:
+                break;
+            case Status.AGGRO_2:
+                break;
+            case Status.DEP_DEF_3:
+                break;
+            case Status.ESQ_4:
+                break;
+            case Status.DEP_MAP_5:
+                break;
+        }
+	}
+}
