603ead08b535847443c0ef6ed68ef710eb85e5aa
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-04-08 16:57:14 +0200

    debut prog switch IA

--- Assets/Scripts/IA.cs
+++ Assets/Scripts/IA.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IA : MonoBehaviour {
+public class IA : BasicCharacter {
 
     public enum Status { NULL_0, DEP_OFF_1, AGGRO_2, DEP_DEF_3, ESQ_4, DEP_MAP_5};
     public Status currentStatus;
+    private GameObject player = GameObject.Find("MainCharacter");
 
-	// Use this for initialization
-	void Start () {
+    private List<Sort> availableSorts;
+
+    // Use this for initialization
+    void Start ()
+    {
         currentStatus = Status.NULL_0;
 
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		switch(currentStatus)
         {
             default:
             case Status.NULL_0:
-                // do stuff
-
-                // check stuff
+                currentStatus = Status.DEP_OFF_1;
                 break;
             case Status.DEP_OFF_1:
+                // on se déplace vers le joueur
+                this.MoveToPoint(player.transform.position);
+
+                availableSorts = IsInRange(player.transform.position);
+
+                if (availableSorts.Count > 0)
+                    currentStatus = Status.AGGRO_2;
+                
                 break;
             case Status.AGGRO_2:
+
                 break;
             case Status.DEP_DEF_3:
                 break;
@@ -35,4 +47,14 @@ public class IA : MonoBehaviour {
                 break;
         }
 	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void MoveToPoint(Vector3 point)
+    {
+        
+    }
 }
