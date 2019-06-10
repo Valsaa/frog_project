96cf0d6db2e801678a86d6086f3b92a8fde2cb19
lemoissonneur <lemoissonneur@users.noreply.github.com>
2019-06-10 13:34:13 +0200

    added fireball scripts

---
+++ Assets/FireBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+
+    public Projectile projectile;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Throw(Vector3 direction)
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+
+    public static void Create()
+    {
+
+    }
+
+}
